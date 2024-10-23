a53a4d697bb78556c40948de600789d8146a9e37
Jonathan Dzul <dzulcarvajaljonathangabriel@gmail.com>
2024-10-23 16:53:54 -0600

    PracticaFinal added

--- lib/pages/home_pages.dart
+++ lib/pages/home_pages.dart
@@ -3,12 +3,19 @@ import 'package:flutter/material.dart';
 import '../utils/my_tab.dart';
 
 
-//  Importar 5 tabs
-import '../tab/Burger_tab.dart';
-import '../tab/Pizza_tab.dart';
+import '../tab/burger_tab.dart';
+import '../tab/pizza_tab.dart';
 import '../tab/donut_tab.dart';
-import '../tab/Smoothie_tab.dart';
-import '../tab/PanCake_tab.dart';
+import '../tab/smoothie_tab.dart';
+import '../tab/pancake_tab.dart';
+
+// 1. Modelo de carrito
+class Cart {
+  int itemCount;
+  double totalAmount;
+
+  Cart({this.itemCount = 0, this.totalAmount = 0.0});
+}
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -18,38 +25,47 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
+  // Lista de tabs
   List<Widget> myTabs = [
-    //donut tab
+    // Donut tab
     const MyTab(
       iconPath: "lib/icons/donut.png",
-      label: "Dona",
-      ),
-    //burger tab
+      label: "Donut",
+    ),
+    // Burger tab
     const MyTab(
       iconPath: "lib/icons/burger.png",
       label: "Burger",
-      ),
-    //smoothie tab
+    ),
+    // Smoothie tab
     const MyTab(
       iconPath: "lib/icons/smoothie.png",
       label: "Smoothie",
-      ),
-    //pancakes tab
+    ),
+    // Pancakes tab
     const MyTab(
       iconPath: "lib/icons/pancakes.png",
       label: "Pancakes",
-      ),
-    //pizza tab
+    ),
+    // Pizza tab
     const MyTab(
       iconPath: "lib/icons/pizza.png",
       label: "Pizza",
-      ),
+    ),
   ];
+
+  Cart cart = Cart(); // Instancia del carrito
+
+  void addToCart(double price) {
+    setState(() {
+      cart.itemCount += 1;
+      cart.totalAmount += price;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return  DefaultTabController(     
-      
+    return DefaultTabController(
       length: myTabs.length,
       child: Scaffold(
         appBar: AppBar(
@@ -61,43 +77,109 @@ class _HomePageState extends State<HomePage> {
               color: Colors.grey[800],
             ),
           ),
-          actions: const [Padding(
-            padding: EdgeInsets.only(right: 24.0),
-            child: Icon(Icons.person),
-          )],
-       ),
-       body: Column(
-        children: [
-          //Texto "I want to eat"
-          const Padding(
-            padding: EdgeInsets.all(24.0),
-            child: Row(
-              children: [
-                Text("I Want to ", style: TextStyle(fontSize: 24,),
-                ),
-                Text("Eat", style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, decoration: TextDecoration.underline),)
-              ],
+          actions: const [
+            Padding(
+              padding: EdgeInsets.only(right: 24.0),
+              child: Icon(Icons.person),
+            )
+          ],
+        ),
+        body: Column(
+          children: [
+            // Texto "I want to eat"
+            const Padding(
+              padding: EdgeInsets.all(24.0),
+              child: Row(
+                children: [
+                  Text(
+                    "I Want to ",
+                    style: TextStyle(
+                      fontSize: 24,
+                    ),
+                  ),
+                  Text(
+                    "Eat",
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      decoration: TextDecoration.underline,
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-          //Tab bar
-          TabBar(tabs: myTabs),
-          //Tab bar view
-           
-           Expanded(child: TabBarView(children: [
-            DonutTab(),
-           const BurgerTab(),
-            const SmoothieTab(),
-            const PanCakeTab(),
-            const PizzaTab()
-          ]
-
-          )
-          
-          )
-      
-          //Total de carrito
-        ],),
+            // Tab bar
+            TabBar(tabs: myTabs),
+            // Tab bar view
+            Expanded(
+              child: TabBarView(
+                children: [
+                  DonutTab(onAddToCart: addToCart), // Pasamos la función
+                  BurgerTab(onAddToCart: addToCart),
+                  SmoothieTab(onAddToCart: addToCart),
+                  PanCakeTab(onAddToCart: addToCart),
+                  PizzaTab(onAddToCart: addToCart),
+                ],
+              ),
+            ),
+            // Total del carrito
+            CartSummary(itemCount: cart.itemCount, totalAmount: cart.totalAmount), // Actualizamos el CartSummary
+          ],
+        ),
       ),
     );
   }
 }
+
+// 2. Widget CartSummary
+class CartSummary extends StatelessWidget {
+  final int itemCount;
+  final double totalAmount;
+
+  const CartSummary({super.key, required this.itemCount, required this.totalAmount});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(24.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              // Texto con cantidad de ítems y precio total
+              Text(
+                "$itemCount Items | \$$totalAmount",
+                style: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              // Botón "View Cart"
+              ElevatedButton(
+                onPressed: () {
+                  // Acción del botón
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.pink, // Color del botón
+                ),
+                child: const Text("View Cart"),
+              ),
+            ],
+          ),
+          const SizedBox(height: 8), // Espacio entre el texto principal y el texto de entrega
+          // Texto de "Delivery Charges Included"
+          const Text(
+            "Delivery Charges Included",
+            style: TextStyle(
+              fontSize: 14,
+              color: Colors.grey,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
--- lib/tab/donut_tab.dart
+++ lib/tab/donut_tab.dart
@@ -2,37 +2,40 @@ import 'package:flutter/material.dart';
 import 'package:p1_donut_app_jonathan_dzul/utils/donut_tile.dart';
 
 class DonutTab extends StatelessWidget {
-  // list of donuts
-final List donutsOnSale =  [
-   // [ donutFlavor, donutPrice, donutColor, imageName ]
-   ["Ice Cream", "36", Colors.blue, "lib/images/icecream_donut.png"],
-   ["Strawberry", "45", Colors.red, "lib/images/strawberry_donut.png"],
-   ["Grape Ape", "84", Colors.purple, "lib/images/grape_donut.png"],
-   ["Choco", "95", Colors.brown, "lib/images/chocolate_donut.png"],
-   ["Ice Cream", "36", Colors.blue, "lib/images/icecream_donut.png"],
-   ["Strawberry", "45", Colors.red, "lib/images/strawberry_donut.png"],
-   ["Grape Ape", "84", Colors.purple, "lib/images/grape_donut.png"],
-   ["Choco", "95", Colors.brown, "lib/images/chocolate_donut.png"],
-];
-   DonutTab({super.key});
+  final List donutsOnSale = [
+    // [ donutFlavor, donutPrice, donutColor, imageName ]
+    ["Ice Cream", "36", Colors.blue, "lib/images/icecream_donut.png"],
+    ["Strawberry", "45", Colors.red, "lib/images/strawberry_donut.png"],
+    ["Grape Ape", "84", Colors.purple, "lib/images/grape_donut.png"],
+    ["Choco", "95", Colors.brown, "lib/images/chocolate_donut.png"],
+    ["Pistachio", "26", Colors.yellow, "lib/images/pistacho_donut.png"],
+    ["Mint", "35", Colors.orange, "lib/images/menta_donut.png"],
+    ["Four", "10", Colors.pink, "lib/images/4donas_donut.png"],
+    ["Six", "72", Colors.green, "lib/images/docena_donut.png"],
+  ];
+
+  final Function(double) onAddToCart; // Callback para agregar al carrito
+
+  DonutTab({super.key, required this.onAddToCart}); // Asegúrate de recibir el callback
 
   @override
   Widget build(BuildContext context) {
-    //Delegate sirve para organizar los widgets en la cuadrícula
     return GridView.builder(
-      itemCount:donutsOnSale.length,
+      itemCount: donutsOnSale.length,
       padding: const EdgeInsets.all(12),
-
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2, childAspectRatio: 1/1.65),
-     itemBuilder: (context, index){
-      return DonutTile(
-        donutFlavor: donutsOnSale[index][0],
-        donutPrice: donutsOnSale[index][1],
-        donutColor: donutsOnSale[index][2],
-        imageName: donutsOnSale[index][3],
-      );
-     });
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 1 / 1.65,
+      ),
+      itemBuilder: (context, index) {
+        return DonutTile(
+          donutFlavor: donutsOnSale[index][0],
+          donutPrice: donutsOnSale[index][1],
+          donutColor: donutsOnSale[index][2],
+          imageName: donutsOnSale[index][3],
+          onAddToCart: () => onAddToCart(double.parse(donutsOnSale[index][1])), // Llama al callback
+        );
+      },
+    );
   }
-  
-  
 }
